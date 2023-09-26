d950d87d5e20ad746f96e13b0db4784bd73151aa
yallenMD <118320346+yallenMD@users.noreply.github.com>
2023-09-26 12:43:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 # Twelve API Key
 api_key = os.getenv('TWELVEDATA_API_KEY')
 # Open AI API Key
-open.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 # 監聽所有來自 /callback 的 Post Request
