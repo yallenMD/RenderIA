2a31d309d635b4ac3b4a1eb2313520da962ef46f
yallenMD <118320346+yallenMD@users.noreply.github.com>
2023-10-19 09:11:25 +0800

    add another colon

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, message)
 
     elif topic_classification(msg)[0] == 'Currency':
-        if topic_classification(msg)[1] != 'N/A'
+        if topic_classification(msg)[1] != 'N/A':
     elif '旋轉木馬' in msg:
         message = Carousel_Template()
         line_bot_api.reply_message(event.reply_token, message)
